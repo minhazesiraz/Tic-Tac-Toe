1ffa82fd28fd492be707f4b6797229ff994c6bb6
minhazesiraz <minhazesiraz@gmail.com>
2025-01-31 00:01:18 +0600

    value

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css";
 
-function Square() {
+function Square({ value }) {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      {value}
+    </button>
+  );
+}
+
+function Board() {
   return (
     <>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-        X
-      </button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-        X
-      </button>
+      <div>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
     </>
   );
 }
 
-export default Square;
+export default Board;
